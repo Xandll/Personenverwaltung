fe763e9407263b04e1290b412840f14f70d2ebdf
Xandll <128627900+Xandll@users.noreply.github.com>
2024-09-05 10:14:25 +0000

    modified:   main.c 	modified:   run     Read File & cut 1st Line

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 typedef struct Person {
@@ -7,35 +8,62 @@ typedef struct Person {
 	char* gender;
 } pers;
 
-void readfile(char **file);
-
-void funktion(pers* p, int *length, int *j, char *i, int *k, char **file);
+void readfile(char** newBuffer);
 
 int main()
 {
-    char* file;
-    int length;
-    int j;
-    char i;
-    int k;
-    readfile(&file);
-    pers p;
-	funktion(&p, &length, &j, &i, &k, &file);
+    char *Data;
+    readfile(&Data);
+    printf("%s", Data);
 	
-	
-	//Ausgabe aller gender
-	/*for (int l = 0; l < k; l++) {
-        printf("Gender von Person %d = %c\n", l + 1, p.gender[l]);
-    }*/
-    
+    printf("\n");
 	return 0;
 }
 
-void readfile(char **file) {
-    *file = "alexander;steiner;m\nmax;musterman;m\nmelanie;quintus;w\nhelene;fische;w\nolaf;scholz;m\n\0";
+void readfile(char** newBuffer) {
+    FILE *fptr;
+    int fsize;
+    fptr = fopen("personen.csv", "r");
+
+    if (NULL == fptr) {printf("ERRROR, could not Open file!\n"); return;}
+
+    fseek(fptr, 0, SEEK_END);
+    fsize = ftell(fptr);
+    fseek(fptr, 0, SEEK_SET);
+
+    char* buffer = calloc(fsize, sizeof(char));    
+
+    int c;
+    int i = 0;
+    while ((c = fgetc(fptr)) != EOF) {
+        buffer[i] = c;
+        i++;
+    }
+    fclose(fptr);
+
+    i = 0;
+    c = buffer[i];
+    while (c != '\n')
+    {
+        i++;
+        c = buffer[i];
+    }
+
+    *newBuffer = (char *)malloc((fsize - i + 1) * sizeof(char));
+    
+    if (*newBuffer == NULL) {
+            printf("ERROR, could not allocate Memory!.\n");
+            return;
+    }
+    strcpy(*newBuffer, buffer + i + 1);
+    
+    printf("%s", *newBuffer);
+
+    free(buffer);
+    free(*newBuffer);
 }
 
-void funktion(pers* p, int *length, int *j, char *i, int *k, char **file) {
+/*void funktion(pers* p, int *length, int *j, char *i, int *k, char **file) {
     *length = 0;
     *j = 0;
     *i = *file[*length];
@@ -51,4 +79,4 @@ void funktion(pers* p, int *length, int *j, char *i, int *k, char **file) {
         (*length)++;
         *i = *file[*length];
     }
-}
+}*/
